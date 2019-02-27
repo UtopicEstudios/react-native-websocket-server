ae2445b47d2fcf9f08364cedb25f3ed33c4b35a7
Carlos Melero <carlos.melero@utopicestudios.com>
2019-02-27 13:33:29 +0100

    Add missing imports... Again

--- android/app/src/main/java/com/unexus/websocketserver/RNWebsocketServerModule.java
+++ android/app/src/main/java/com/unexus/websocketserver/RNWebsocketServerModule.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.LifecycleEventListener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
